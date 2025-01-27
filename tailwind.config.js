f05c09e54eae0ef3776c241fcefe89f59686e25d
mdshoib <mdshoibakhtar1000@gmail.com>
2025-01-27 11:57:43 +0530

    first commit talwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        'color-left':'#ff7129',
+        'color-right':'#cca907',
+      },
+    },
+  },
+  plugins: [],
+}
+
